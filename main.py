4a0c14b494225eb22df6f5a3d8caa0b091a55067
PR0VERB <36727013+PR0VERB@users.noreply.github.com>
2022-12-01 12:28:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-import streamlit
+import streamlit as st
 from src import (
     upload_files_to_gdrive,
     list_out_file_from_gdrive,
@@ -8,14 +8,16 @@ from src import (
     create_file_and_write_text_init,
     )
 
+but = st.button("hi")
+if but:
+    
+    gauth = GoogleAuth()           
+    drive = GoogleDrive(gauth)
+    driveId = '1yotY0YQUaq6orkEzJTUmq-13EuP9ZfDp' #folderId og Google Drive you are working with
+    upload_file_list = ['samplefile1.txt', 'samplefile2.txt'] #List of file to be uploaded.
 
-gauth = GoogleAuth()           
-drive = GoogleDrive(gauth)
-driveId = '1yotY0YQUaq6orkEzJTUmq-13EuP9ZfDp' #folderId og Google Drive you are working with
-upload_file_list = ['samplefile1.txt', 'samplefile2.txt'] #List of file to be uploaded.
 
-
-upload_files_to_gdrive(upload_file_list, drive, driveId)
-file_list_in_drive = list_out_file_from_gdrive(drive, driveId) #List of files to List from gdrive
-download_files_from_gdrive(file_list_in_drive)
-create_file_and_write_text_init(drive, driveId)
+    upload_files_to_gdrive(upload_file_list, drive, driveId)
+    file_list_in_drive = list_out_file_from_gdrive(drive, driveId) #List of files to List from gdrive
+    download_files_from_gdrive(file_list_in_drive)
+    create_file_and_write_text_init(drive, driveId)
