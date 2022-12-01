5b9a33b99e8a15dfe0e08debca20ba869f8fcafe
PR0VERB <36727013+PR0VERB@users.noreply.github.com>
2022-12-01 12:13:18 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import streamlit
+from src import (
+    upload_files_to_gdrive,
+    list_out_file_from_gdrive,
+    download_files_from_gdrive,
+    create_file_and_write_text_init,
+    )
+
+
+gauth = GoogleAuth()           
+drive = GoogleDrive(gauth)
+driveId = '1yotY0YQUaq6orkEzJTUmq-13EuP9ZfDp' #folderId og Google Drive you are working with
+upload_file_list = ['samplefile1.txt', 'samplefile2.txt'] #List of file to be uploaded.
+
+
+upload_files_to_gdrive(upload_file_list, drive, driveId)
+file_list_in_drive = list_out_file_from_gdrive(drive, driveId) #List of files to List from gdrive
+download_files_from_gdrive(file_list_in_drive)
+create_file_and_write_text_init(drive, driveId)
